180e193144b42feacd6ead359d0f29aa14abf57b
Leamida <leoanangm@gmail.com>
2022-04-09 07:37:35 +0700

    starter project

---
+++ app/src/main/java/com/example/storyapp/presentation/ui/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.storyapp.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.storyapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
