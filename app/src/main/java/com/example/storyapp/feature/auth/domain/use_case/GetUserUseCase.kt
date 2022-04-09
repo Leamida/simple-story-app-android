46d0a90d87d21a120876ecdb2583fe37ab3e8a54
Leamida <leoanangm@gmail.com>
2022-04-09 20:31:09 +0700

    Added user pref feature

--- app/src/main/java/com/example/storyapp/feature/auth/domain/use_case/GetUserUseCase.kt
+++ app/src/main/java/com/example/storyapp/feature/auth/domain/use_case/GetUserUseCase.kt
@@ -1,4 +1,17 @@
 package com.example.storyapp.feature.auth.domain.use_case
 
-class GetUserUseCase {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
+import com.example.storyapp.feature.auth.data.repository.UserRepository
+import com.example.storyapp.feature.auth.domain.model.User
+import com.example.storyapp.core.util.Result
+import javax.inject.Inject
+
+class GetUserUseCase @Inject constructor(
+    private val userRepository: UserRepository
+) {
+    operator fun invoke():LiveData<User?> = liveData {
+        emitSource(userRepository.getUser().asLiveData())
+    }
 }
