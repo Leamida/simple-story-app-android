737a19eb206f0939c75071d3ceb8fe682df719b3
Leamida <leoanangm@gmail.com>
2022-04-09 20:18:07 +0700

    Added user pref feature

--- app/src/main/java/com/example/storyapp/di/AppModule.kt
+++ app/src/main/java/com/example/storyapp/di/AppModule.kt
@@ -2,8 +2,11 @@ package com.example.storyapp.di
 
 import com.example.storyapp.BuildConfig
 import com.example.storyapp.feature.auth.data.repository.AuthRepository
+import com.example.storyapp.feature.auth.data.repository.UserRepository
 import com.example.storyapp.feature.auth.data.source.api.AuthApiService
+import com.example.storyapp.feature.auth.data.source.local.preferences.UserPreferences
 import com.example.storyapp.feature.auth.domain.repository.AuthRepositoryService
+import com.example.storyapp.feature.auth.domain.repository.UserRepositoryService
 import com.example.storyapp.feature.story.data.repository.StoryRepository
 import com.example.storyapp.feature.story.data.source.api.StoryApiService
 import com.example.storyapp.feature.story.domain.repository.StoryRepositoryService
@@ -74,5 +77,11 @@ object AppModule {
     @Singleton
     fun provideAuthRepository(api: AuthApiService): AuthRepositoryService {
         return AuthRepository(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserRepository(api: UserPreferences): UserRepositoryService {
+        return UserRepository(api)
     }
 }
---
+++ app/src/main/java/com/example/storyapp/feature/auth/data/repository/UserRepository.kt
@@ -0,0 +1,20 @@
+package com.example.storyapp.feature.auth.data.repository
+
+import com.example.storyapp.feature.auth.data.source.local.preferences.UserPreferences
+import com.example.storyapp.feature.auth.data.source.local.preferences.UserPreferencesService
+import com.example.storyapp.feature.auth.domain.model.User
+import com.example.storyapp.feature.auth.domain.repository.UserRepositoryService
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class UserRepository @Inject constructor(
+    private val userPreferences: UserPreferences
+) :UserRepositoryService{
+    override fun getUser(): Flow<User?> {
+        return userPreferences.getUser()
+    }
+
+    override suspend fun setUser(user: User) {
+        return userPreferences.setUser(user)
+    }
+}
